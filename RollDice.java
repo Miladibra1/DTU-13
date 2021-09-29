7107a4cf79ca74910ddaebc7925604c982b1ac3e
Mudi26 <91070290+Mudi26@users.noreply.github.com>
2021-09-29 13:08:46 +0200

    Update RollDice.java

--- RollDice.java
+++ RollDice.java
@@ -45,3 +45,53 @@ public class RollDice {
 
     }
 }
+
+
+
+NY-NY-NY-NY-NY-NY-NY-NY-NY
+
+import java.util.Scanner;
+
+
+public class CDIO_1ny {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Slå terningerne");
+        int player1, player2;
+        player1 = 0;
+        player2 = 0;
+        int Die1, Die2, Die3, Die4;
+        Die1 = (int) (Math.random() * 6) + 1;
+        Die2 = (int) (Math.random() * 6) + 1;
+        Die3 = (int) (Math.random() * 6) + 1;
+        Die4 = (int) (Math.random() * 6) + 1;
+        int sum1, sum2;
+        sum1 = (player1 + (Die1 + Die2));
+        sum2 = (player2 + (Die3 + Die4));
+        System.out.println("Spiller1 får i sine 2 kast en samlet værdi på: " + sum1 + ", Spiller2 får i sine 2 kast en samlet værdi på: " + sum2);
+        while (true) {
+            if (sum1 > sum2) {
+                System.out.println(sum1 + " Er større end " + sum2 + " Derfor vinder Spiller1 denne runde");
+            } else if (sum2 > sum1) {
+                System.out.println(sum1 + " Er mindre end " + sum2 + " Derfor vinder Spiller2 denne runde");
+            } else if (sum1 == sum2) {
+                System.out.println(sum1 + " Er lig med " + sum2 + " Derfor bliver denne runde uafgjort");
+            }
+            if (sum1 >= 40 || sum2 >= 40); {
+                System.out.println("The game is over");
+            } break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
