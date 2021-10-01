4feabf016b02be94126762e35a3cf9e4d656b626
Miladibra1 <91473417+Miladibra1@users.noreply.github.com>
2021-10-01 11:08:50 +0200

    Update RollDice.java

--- RollDice.java
+++ RollDice.java
@@ -1,3 +1,62 @@
+NY-NY-NY 
+import java.util.Scanner;
+
+public class Jam {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Slå terningerne, den der har flest point når de når 40+, VINDER. Held og Lykke!");
+        int Spiller1, Spiller2;
+        Spiller1 = 0;
+        Spiller2 = 0;
+        int Die1, Die2, Die3, Die4;
+        int sum1, sum2;
+        int runde = 1;
+
+        while (Spiller1 < 40 && Spiller2 < 40) {
+            Die1 = (int) (Math.random() * 6) + 1;
+            Die2 = (int) (Math.random() * 6) + 1;
+            Die3 = (int) (Math.random() * 6) + 1;
+            Die4 = (int) (Math.random() * 6) + 1;
+            sum1 = (Die1 + Die2);
+            sum2 = (Die3 + Die4);
+
+            Spiller1 += sum1;
+            Spiller2 += sum2;
+
+            System.out.println("------------Runde " + runde + "-------------");
+            runde++;
+
+            System.out.println("Spiller1 får i sine 2 kast en samlet værdi på: " + sum1);
+            System.out.println("Spiller2 får i sine 2 kast en samlet værdi på: " + sum2);
+
+            if (sum1 > sum2) {
+                System.out.println(sum1 + " Er større end " + sum2 + " og derfor vinder spiller1 denne runde. Spiller1 har nu " + Spiller1 + " point, mens Spiller2 har " + Spiller2 + " point");
+            } else if (sum2 > sum1) {
+                System.out.println(sum2 + " Er større end " + sum1 + " og derfor vinder spiller2 denne runde. Spiller 1 har nu " + Spiller1 + " point, mens Spiller2 har " + Spiller2 + " point");
+            } else if (sum1 == sum2) {
+                System.out.println(sum1 + " Er lig med " + sum2 + ", og denne runde bliver altså til et uafgjort, nu har Spiller1 " + Spiller1 + " point, mens Spiller2 har " + Spiller2 + " point");
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 public class RollDice {
     public static void main(String[] args) {
